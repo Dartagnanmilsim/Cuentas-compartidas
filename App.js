e37873d5e6914578a6a9814e1e141b2d54799c14
Dartagnanmilsim <wleon_195@hotmail.com>
2026-02-17 23:45:21 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfMEoJ0yuS9EE1UC8cWHpqjgSL0bphcqs",
+  authDomain: "gastos-parche.firebaseapp.com",
+  projectId: "gastos-parche",
+  storageBucket: "gastos-parche.firebasestorage.app",
+  messagingSenderId: "558910304272",
+  appId: "1:558910304272:web:8bedfef13b39c19a8b9a95"
+};
