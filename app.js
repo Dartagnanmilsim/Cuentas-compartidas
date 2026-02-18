b0f20055c1c92f2504d5a9037c5f74f0e937ee22
Dartagnanmilsim <wleon_195@hotmail.com>
2026-02-18 02:15:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { 
-  getFirestore, collection, addDoc, getDocs, deleteDoc, doc 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBfMEoJ0yuS9EE1UC8cWHpqjgSL0bphcqs",
   authDomain: "gastos-parche.firebaseapp.com",
@@ -12,8 +7,5 @@ const firebaseConfig = {
   appId: "1:558910304272:web:e9ae826d11a7865a8b9a95"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-window.db = db;
-window.fb = { collection, addDoc, getDocs, deleteDoc, doc };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
